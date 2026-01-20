fd44fd1143983dd46a90806457f7b474ac59c77a
Daniel Lerche <daniel.lerche10@gmail.com>
2026-01-20 09:51:35 +0100

    Added AES-256 for testing

--- uno-bench/src/base_bench.cpp
+++ uno-bench/src/base_bench.cpp
@@ -1,10 +1,13 @@
+#include <avr/sleep.h>
 #include <Arduino.h>
 
 // =============================================================
 // SETUP (Runs ONCE per board reset)
 // =============================================================
 void setup() {
-
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    sleep_cpu();
 }
 
 void loop() {
